351efba280934336e46c67c99e5dc9d80f551a5c
migu <migu@fanhaoyue.com>
2019-07-24 08:06:54 +0800

    添加二维码页面

---
+++ pages/my-qr/my-qr.js
@@ -0,0 +1,12 @@
+const placeholderUrl = 'https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/cache/static/protocol/https/home/img/qrcode/zbios_x2_5869f49.png';
+
+Page({
+  data: {
+    name: '疯趣的二师兄',
+    qrUrl: placeholderUrl,
+    userLogoUrl: placeholderUrl
+  },
+  onLoad: function () {
+
+  }
+})
