0b1f4f92bb1d9a57c41b9d92c6f454ba9c2f6515
migu <migu@fanhaoyue.com>
2019-09-19 08:39:24 +0800

    修改授权页面bug

--- utils/goUserHome.js
+++ utils/goUserHome.js
@@ -3,15 +3,15 @@ import UserRole from './userRole';
 
 export default function(userRole){
 	if (`${userRole}` === UserRole['customer']){ // 消费者
-		router.navigateTo({
+		router.redirectTo({
 			url: `/pages/index/index`
 		});
 	} else if(`${userRole}` === UserRole['doctor']){ // 医生
-		router.navigateTo({
+		router.redirectTo({
 			url: `/pages/doctor/doctor`
 		})
 	} else if (`${userRole}` === UserRole['medical']){// 医药代表
-		router.navigateTo({
+		router.redirectTo({
 			url: `/pages/medical-home/home`
 		});
 	} else if (`${userRole}` === UserRole['courier']){ // 派送员
