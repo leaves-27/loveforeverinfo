76144ad624bd5bd13c26f957c743605ef5d33820
migu <migu@fanhaoyue.com>
2019-09-22 12:43:59 +0800

    接口联调

--- utils/request.js
+++ utils/request.js
@@ -3,8 +3,8 @@ import { redirect } from "./route/index";
 import router from './router';
 
 const request = new Request();
-const count = 0; // 自动登录尝试请求的次数
-const MAX_COUNT = 6; // 自动登录最大尝试请求次数
+// const count = 0; // 自动登录尝试请求的次数
+// const MAX_COUNT = 6; // 自动登录最大尝试请求次数
 /**
  * 请求拦截器
  * */
@@ -30,15 +30,38 @@ request.beforeRequest = function({
 };
 
 request.afterRequest  = function (result, next){
-	const { request, response } = result;
-	const { code } = response;
-	if(code === 'session_out'){ // 是token过期的话，跳转到授权登录页重新授权登录
-		router.navigateTo({
-			url: redirect
-		})
-	} else {
-		next();
+	const { request, response, errMsg = '' } = result;
+
+	if(!!errMsg){ // 请求失败
+		// 若请求超时，则给用户提示让其稍后再试
+		if (errMsg === 'request:fail timeout'){
+			wx.showToast({
+				icon:'none',
+				title: '响应超时，请重新操作'
+			});
+			return;
+		}
+	} else { // 请求成功
+		const { data } = response;
+		const { code, statusCode } = data;
+		// 若token过期，则跳转到授权登录页重新授权登录
+		if(code === 'session_out'){
+			const count = wx.getStorageSync('tokenCount') || 1;
+			if(count * 1 === 6){
+				wx.setStorageSync('tokenCount', 0);
+				return;
+			}
+			wx.setStorageSync('token', '');
+			wx.setStorageSync('userRole', '');
+			wx.setStorageSync('tokenCount', count + 1);
+			router.navigateTo({
+				url: redirect
+			});
+			return;
+		}
 	}
+
+	next();
 };
 
 export default request;
--- utils/router.js
+++ utils/router.js
@@ -3,22 +3,25 @@ import route from "./route/index";
 
 const router = new Router(route);
 router.beforeCreate = function(url, next){
-	const { routes = [], redirect} = this.route;
+	const { routes = [], redirect } = this.route;
 	const route = routes.find((item)=>{
 		const { path = '' } = item;
 		const regExp = new RegExp(path);
 		return regExp.test(url);
 	});
 
-	const currentUserRole = wx.getStorageSync('userRole');
-	const { role = [] } = route;
-	const index = role.indexOf(`${currentUserRole}`);
-	if (role.length === 0 || index > -1){
-		next();
-	} else {
-		wx.redirectTo({
-			url: redirect
-		})
+	if (route){
+		const currentUserRole = wx.getStorageSync('userRole');
+		const { role = [] } = route;
+		const index = role.indexOf(`${currentUserRole}`);
+		if (role.length === 0 || index > -1){
+			next();
+			return;
+		}
 	}
+
+	wx.redirectTo({
+		url: redirect
+	});
 };
 export default router;
